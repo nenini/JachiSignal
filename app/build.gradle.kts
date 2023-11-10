03d181ddc06bcce2daaff482545b00e1c1695bed
sehyunnnn <kimsehyen7941@naver.com>
2023-11-10 21:17:05 +0900

    sign up 으로 이동하기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.jachisignal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.jachisignal"
--- app/src/main/java/com/example/jachisignal/MainActivity.java
+++ app/src/main/java/com/example/jachisignal/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.jachisignal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView btn_signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("hh");
 
         setContentView(R.layout.activity_main);
+
+        btn_signup = findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
