f91c75f3b42c3562b205ef39df965a77513db419
nenini <kyr068600@gmail.com>
2023-11-10 18:55:56 +0900

    로그인 화면 xml

--- app/src/main/java/com/example/jachisignal/MainActivity.java
+++ app/src/main/java/com/example/jachisignal/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         System.out.println("hh");
+
         setContentView(R.layout.activity_main);
 
     }
