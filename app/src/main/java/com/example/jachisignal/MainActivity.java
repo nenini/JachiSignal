6c3cad194e8995015a261a62a8dcdcd9782cf1b2
kangsumin1 <ksm030305@naver.com>
2023-11-10 22:59:18 +0900

    이제 로그인됨

--- app/src/main/java/com/example/jachisignal/MainActivity.java
+++ app/src/main/java/com/example/jachisignal/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.jachisignal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jachisignal.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.StartupTime;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView btn_signup;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
 
         binding.passwordFind.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +48,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.emailEditTxtMain.getText().toString();
+                String password = binding.passwordEditTxtMain.getText().toString();
+
+                signIn(email,password);
+            }
+        });
+    }
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("kkk", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Log.d("kkk", "signInWithEmail:failure");
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+    public void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.putExtra("USER_PROFILE", "email: " + user.getEmail() + "\n" + "uid: " + user.getUid());
+
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/jachisignal/SignupActivity.java
+++ app/src/main/java/com/example/jachisignal/SignupActivity.java
@@ -62,9 +62,9 @@ public class SignupActivity extends AppCompatActivity {
                 });
     }
 
-    private void updateUI(FirebaseUser user) {
+    public void updateUI(FirebaseUser user) {
         if (user != null) {
-            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent = new Intent(this, HomeActivity.class);
             intent.putExtra("USER_PROFILE", "email: " + user.getEmail() + "\n" + "uid: " + user.getUid());
 
             startActivity(intent);
