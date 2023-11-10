686a7610fdf1c6c10c26d4c2c6f54c992b44390c
sehyunnnn <kimsehyen7941@naver.com>
2023-11-10 19:44:56 +0900

    아이디 찾기 xml

---
+++ app/src/main/java/com/example/jachisignal/FindidActivity.java
@@ -0,0 +1,14 @@
+package com.example.jachisignal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findid);
+    }
+}
