afec1876496ad08e01fa58b22602a5a76a43cf40
kangsumin1 <ksm030305@naver.com>
2023-11-10 21:46:54 +0900

    login added 2

--- app/src/main/java/com/example/jachisignal/MainActivity.java
+++ app/src/main/java/com/example/jachisignal/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         binding.passwordFind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,FindPasswwodActivity.class));
+                startActivity(new Intent(MainActivity.this, FindPasswordActivity.class));
             }
         });
 
--- app/src/main/java/com/example/jachisignal/SignupActivity.java
+++ app/src/main/java/com/example/jachisignal/SignupActivity.java
@@ -33,24 +33,24 @@ public class SignupActivity extends AppCompatActivity {
                 String email = binding.emailEditTxt.getText().toString();
                 String password = binding.passwordEditTxt.getText().toString();
 
-                signIn(email,password);
+                signUp(email,password);
             }
         });
     }
-    private void signIn(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password)
+    private void signUp(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d("TAG", "signInWithEmail:success");
+                            Log.d("TAG", "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
-                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Log.d("TAG", "signInWithEmail:failure");
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Log.d("TAG", "createUserWithEmail:failure");
                             Toast.makeText(getApplicationContext(), "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
