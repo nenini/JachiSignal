266d000a4b8f2b17b399d1a00fbeeba7a0ede3a2
nenini <kyr068600@gmail.com>
2023-11-14 23:14:18 +0900

    fragment 1,2,3

--- app/src/main/java/com/example/jachisignal/FragmentHome.java
+++ app/src/main/java/com/example/jachisignal/FragmentHome.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+
+import com.example.jachisignal.fragmentHome.FragmentHome1;
+import com.example.jachisignal.fragmentHome.FragmentHome2;
+import com.example.jachisignal.fragmentHome.FragmentHome3;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -13,6 +18,9 @@ import androidx.fragment.app.Fragment;
  * create an instance of this fragment.
  */
 public class FragmentHome extends Fragment {
+    FragmentHome1  fragmentHome1;
+    FragmentHome2 fragmentHome2;
+    FragmentHome3 fragmentHome3;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -57,7 +65,34 @@ public class FragmentHome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.fragment_home,container,false) ;
+        fragmentHome1=new FragmentHome1();
+        fragmentHome2=new FragmentHome2();
+        fragmentHome3=new FragmentHome3();
+
+        ImageView gonggu_face=rootView.findViewById(R.id.gonggu_face);
+        gonggu_face.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getChildFragmentManager().beginTransaction().replace(R.id.fragment_home_tab, fragmentHome1).commit();
+            }
+        });
+        ImageView gonggu_del=rootView.findViewById(R.id.gonggu_deli);
+        gonggu_del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getChildFragmentManager().beginTransaction().replace(R.id.fragment_home_tab, fragmentHome2).commit();
+            }
+        });
+        ImageView playing=rootView.findViewById(R.id.playing);
+        playing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getChildFragmentManager().beginTransaction().replace(R.id.fragment_home_tab, fragmentHome3).commit();
+            }
+        });
+
+
+        return rootView;
     }
 }
