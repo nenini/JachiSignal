f5e450acec77481ffd8afdb1f8aa2aebc1a5a4cf
kangsumin1 <ksm030305@naver.com>
2023-11-20 18:36:49 +0900

    11월 20일 알고리즘 수업시간에....

---
+++ app/src/main/java/com/example/jachisignal/GongguWritingActivity.java
@@ -0,0 +1,14 @@
+package com.example.jachisignal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GongguWritingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gonggu_writing);
+    }
+}
