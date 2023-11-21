2ecec3d5db6ad90d5caa3eb1980ad7fe1ff6bddb
sehyunnnn <kimsehyen7941@naver.com>
2023-11-21 17:57:22 +0900

    게시물 내부 완성

---
+++ app/src/main/java/com/example/jachisignal/Post_Inside_09.java
@@ -0,0 +1,13 @@
+package com.example.jachisignal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Post_Inside_09 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_inside09);
+    }
+}
