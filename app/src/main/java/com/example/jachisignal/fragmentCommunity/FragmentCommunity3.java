cfa549f465ad57d4079347bd2a322dc263101d3c
nenini <kyr068600@gmail.com>
2023-11-27 19:30:28 +0900

    레시피 버튼 intent

--- app/src/main/java/com/example/jachisignal/fragmentCommunity/FragmentCommunity3.java
+++ app/src/main/java/com/example/jachisignal/fragmentCommunity/FragmentCommunity3.java
@@ -1,14 +1,23 @@
 package com.example.jachisignal.fragmentCommunity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import com.example.jachisignal.FindPasswordActivity;
+import com.example.jachisignal.MainActivity;
 import com.example.jachisignal.R;
+import com.example.jachisignal.WritingActivity.RecipeWritingActivity;
+import com.example.jachisignal.fragmentHome.FragmentHome2;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -62,5 +71,18 @@ public class FragmentCommunity3 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_community3, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+        ImageButton recipe_BTN=view.findViewById(R.id.community3_write_btn);
+        recipe_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), RecipeWritingActivity.class));
+            }
+        });
     }
 }
