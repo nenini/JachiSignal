c706f7b43d7b6f6a1c7881d2ed7453107663797e
kangsumin1 <ksm030305@naver.com>
2023-11-28 17:40:58 +0900

    비밀번호 재설정 완료

--- app/src/main/java/com/example/jachisignal/AppUser.java
+++ app/src/main/java/com/example/jachisignal/AppUser.java
@@ -1,5 +1,69 @@
 package com.example.jachisignal;
 
 public class AppUser {
+    private String name;
+    private String nickname;
+    private String email;
+    private String pw;
+    private String address;
+    private String phone;
 
+    public AppUser(String email, String pw, String name, String phone, String address, String nickname) {
+        this.email=email;
+        this.name=name;
+        this.nickname = nickname;
+        this.pw = pw;
+        this.phone = phone;
+        this.address=address;
+    }
+
+    public AppUser(){}
+
+    public String getPw() {
+        return pw;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
 }
--- app/src/main/java/com/example/jachisignal/ResetPasswordActivity.java
+++ app/src/main/java/com/example/jachisignal/ResetPasswordActivity.java
@@ -9,16 +9,23 @@ import android.widget.Toast;
 
 import com.example.jachisignal.databinding.ActivityResetPasswordBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+
+    String TAG = "ResetPasswordActivity";
+    AppUser appUser;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +33,51 @@ public class ResetPasswordActivity extends AppCompatActivity {
         ActivityResetPasswordBinding binding = ActivityResetPasswordBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = db.collection("users").document(user.getEmail());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                appUser = documentSnapshot.toObject(AppUser.class);
+            }
+        });
+
         binding.resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 String presentPassword = binding.presentPW.getText().toString();
                 String newPassword = binding.newPW.getText().toString();
                 String passwordCheck = binding.newPW2.getText().toString();
 
-//                FirebaseFirestore db = FirebaseFirestore.getInstance();
-//                CollectionReference users = db.collection("users");
-//                DocumentReference userDoc = users.document(user.getEmail());
-//                if(userDoc.get()) {
-//
-//                }
-                if(newPassword.equals(passwordCheck)) {
+                if(!appUser.getPw().equals(presentPassword)) {
+                    Toast.makeText(getApplicationContext(), "현재 비밀번호가 틀렸습니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if(newPassword.equals(passwordCheck)) {
                     user.updatePassword(newPassword)
                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
-                                        Log.d("TAG", "User password updated.");
+                                        Log.d("TAG", "비밀번호 확인이 틀렸습니다.");
+                                        finish();
+                                        docRef.update("pw", newPassword)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.w(TAG, "Error updating document", e);
+                                                    }
+                                                });
+
                                     }
                                 }
                             });
