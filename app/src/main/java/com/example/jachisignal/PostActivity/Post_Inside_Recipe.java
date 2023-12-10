c6c503cf684b3e78acd4f509a3ade08b3a136e5a
nenini <kyr068600@gmail.com>
2023-12-11 00:52:20 +0900

    예린 버전 연습

--- app/src/main/java/com/example/jachisignal/PostActivity/Post_Inside_Recipe.java
+++ app/src/main/java/com/example/jachisignal/PostActivity/Post_Inside_Recipe.java
@@ -1,9 +1,13 @@
 package com.example.jachisignal.PostActivity;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,6 +16,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.jachisignal.AppUser;
@@ -20,6 +26,7 @@ import com.example.jachisignal.Doc.ChatHolder;
 import com.example.jachisignal.Doc.NestedChat;
 import com.example.jachisignal.Doc.NestedChatHolder;
 import com.example.jachisignal.Doc.RecipeDoc;
+import com.example.jachisignal.Doc.RecipeDocHolder;
 import com.example.jachisignal.R;
 import com.example.jachisignal.databinding.ActivityPostInsideCommunityBinding;
 import com.example.jachisignal.databinding.ActivityPostInsideRecipeBinding;
@@ -33,10 +40,12 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.auth.User;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Post_Inside_Recipe extends AppCompatActivity {
@@ -53,6 +62,11 @@ public class Post_Inside_Recipe extends AppCompatActivity {
     private FirestoreRecyclerAdapter adapter;
     private FirestoreRecyclerAdapter adapterNestedChat;
 
+    private FirestoreRecyclerAdapter adapter_user;
+
+    String Id;
+    String writeID;
+
 
 
     @Override
@@ -60,12 +74,6 @@ public class Post_Inside_Recipe extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityPostInsideRecipeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
         Intent intent = getIntent();
         String collectionName = intent.getStringExtra("COLLECTION");
         documentName = intent.getStringExtra("DOCUMENT");
@@ -78,6 +86,10 @@ public class Post_Inside_Recipe extends AppCompatActivity {
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                 recipeDoc = documentSnapshot.toObject(RecipeDoc.class);
                 Log.d("KSM", "onSuccess: ");
+
+                // 세현
+                Id = recipeDoc.getWriteId();
+                //세현
                 binding.titleRecipePost.setText(recipeDoc.getContentTitle());
 
                 binding.textRecipePost.setText(recipeDoc.getText());
@@ -267,7 +279,184 @@ public class Post_Inside_Recipe extends AppCompatActivity {
 
             }
         });
+
+        binding.nicknameRecipePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCustomDialog();
+
+            }
+        });
     }
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String writingNum[];
+    String text1="99";
+    String text2="99";
+    String text3="99";
+    private void showCustomDialog() {
+        Dialog dialog = new Dialog(Post_Inside_Recipe.this); // 다이얼로그 생성
+        dialog.setContentView(R.layout.custom_user); // 사용자 지정 다이얼로그 레이아웃 설정
+
+        ImageView dialogImageView = dialog.findViewById(R.id.user_inf_img);
+        TextView dialogUserNickname = dialog.findViewById(R.id.user_inf_nickname);
+        ImageView dialogCloseClick = dialog.findViewById(R.id.closeClick_img);
+        TextView dialogUserText1 = dialog.findViewById(R.id.text1);
+        TextView dialogUserText2 = dialog.findViewById(R.id.text2);
+        TextView dialogUserText3 = dialog.findViewById(R.id.text3);
+
+
+        db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = db.collection("users").document(user.getEmail());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                appUser = documentSnapshot.toObject(AppUser.class);
+                List<String> list = appUser.getMyWrite();
+                int size = list.size();
+                Log.d("test",Integer.toString(size));
+                int num = size - 3;
+                for (int i = size; i > num; i--) {
+                    if (i <=0) {
+                        break;
+                    } else if (i == num+3) {
+                        writingNum = list.get(i - 1).split("_");
+                        dialogUserText1.setText(writingNum[1]);
+                        text1=writingNum[0];
+                    } else if (i == num +2) {
+                        writingNum = list.get(i - 1).split("_");
+                        dialogUserText2.setText(writingNum[1]);
+                        text2=writingNum[0];
+                    } else if (i == num+1) {
+                        writingNum = list.get(i - 1).split("_");
+                        dialogUserText3.setText(writingNum[1]);
+                        text3=writingNum[0];
+                    }
+                }
+                dialogUserText1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(text1.compareTo("1")==0){
+                            Intent intent = new Intent(v.getContext(), Post_Inside_09.class);
+                            intent.putExtra("COLLECTION", "gongu1Writings");
+                            intent.putExtra("DOCUMENT", dialogUserText1.getText().toString());
+                            v.getContext().startActivity(intent);
+                        } else if (text1.compareTo("2")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_09_2.class);
+                            intent.putExtra("COLLECTION", "gongu2Writings");
+                            intent.putExtra("DOCUMENT", dialogUserText1.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text1.compareTo("3")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Playing.class);
+                            intent.putExtra("COLLECTION", "leisureWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText1.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }
+                        else if (text1.compareTo("4")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Community.class);
+                            intent.putExtra("COLLECTION", "communityWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText1.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text1.compareTo("5")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Jachitem.class);
+                            intent.putExtra("COLLECTION", "jachitemWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText1.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text1.compareTo("6")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Recipe.class);
+                            intent.putExtra("COLLECTION", "recipeWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText1.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }
+                        else ;
+
+                    }
+                });
+                dialogUserText2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(text2.compareTo("1")==0){
+                            Intent intent = new Intent(v.getContext(), Post_Inside_09.class);
+                            intent.putExtra("COLLECTION", "gongu1Writings");
+                            intent.putExtra("DOCUMENT", dialogUserText2.getText().toString());
+                            v.getContext().startActivity(intent);
+                        } else if (text2.compareTo("2")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_09_2.class);
+                            intent.putExtra("COLLECTION", "gongu2Writings");
+                            intent.putExtra("DOCUMENT", dialogUserText2.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text2.compareTo("3")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Playing.class);
+                            intent.putExtra("COLLECTION", "leisureWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText2.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }
+                        else if (text2.compareTo("4")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Community.class);
+                            intent.putExtra("COLLECTION", "communityWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText2.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text2.compareTo("5")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Jachitem.class);
+                            intent.putExtra("COLLECTION", "jachitemWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText2.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text2.compareTo("6")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Recipe.class);
+                            intent.putExtra("COLLECTION", "recipeWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText2.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else;
+                    }
+                });
+                dialogUserText3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(text3.compareTo("1")==0){
+                            Intent intent = new Intent(v.getContext(), Post_Inside_09.class);
+                            intent.putExtra("COLLECTION", "gongu1Writings");
+                            intent.putExtra("DOCUMENT", dialogUserText3.getText().toString());
+                            v.getContext().startActivity(intent);
+                        } else if (text3.compareTo("2")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_09_2.class);
+                            intent.putExtra("COLLECTION", "gongu2Writings");
+                            intent.putExtra("DOCUMENT", dialogUserText3.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text3.compareTo("3")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Playing.class);
+                            intent.putExtra("COLLECTION", "leisureWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText3.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }
+                        else if (text3.compareTo("4")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Community.class);
+                            intent.putExtra("COLLECTION", "communityWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText3.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text3.compareTo("5")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Jachitem.class);
+                            intent.putExtra("COLLECTION", "jachitemWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText3.getText().toString());
+                            v.getContext().startActivity(intent);
+                        }else if (text3.compareTo("6")==0) {
+                            Intent intent = new Intent(v.getContext(), Post_Inside_Recipe.class);
+                            intent.putExtra("COLLECTION", "recipeWritings");
+                            intent.putExtra("DOCUMENT", dialogUserText3.getText().toString());
+                            v.getContext().startActivity(intent);
+                        } else;
+                    }
+                });
+
+            }
+
+        });
+        dialog.show();
+
+
+    }
+
+
+
 
     private void downloadImageTo(String uri) {
         // Get a default Storage bucket
@@ -351,6 +540,8 @@ public class Post_Inside_Recipe extends AppCompatActivity {
 
     }
 
+
+
     private boolean hasSignedIn() {
         return FirebaseAuth.getInstance().getCurrentUser() != null ? true : false;
     }
@@ -362,6 +553,7 @@ public class Post_Inside_Recipe extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         adapter.startListening();
+
     }
 
     @Override
